afdf8af10388eb6153b0344ed9628d917e921835
Xavier Niu <xavierniu@Xaviers-MacBook-Pro.local>
2020-11-17 20:59:00 +0800

    sync

---
+++ src/ch11/custom_type_test/custom_type_test.go
@@ -0,0 +1,28 @@
+package custom_type_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type IntConv func(op int) int
+
+func timeSpent(inner IntConv) IntConv {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)xx,
+		fmt.Println("time spent: ", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFun(op int) int {
+	time.Sleep(time.Second * 2)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	tsSF := timeSpent(slowFun)
+	t.Log(tsSF(10))
+}
