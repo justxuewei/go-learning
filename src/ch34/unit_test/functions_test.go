b3d2fc1e6d8c896623ae65a26a3c9b4da0b92f3b
xavier <a@nxw.name>
2020-11-25 09:39:13 +0800

    ch33 ch34

---
+++ src/ch34/unit_test/functions_test.go
@@ -0,0 +1,18 @@
+package unit_test
+
+import "testing"
+
+func square(i int) int {
+	return i * i
+}
+
+func TestSquare(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		if ret != expected[i] {
+			t.Errorf("input is %d, the expected is %d, the actual is %d", inputs[i], expected[i], ret)
+		}
+	}
+}
